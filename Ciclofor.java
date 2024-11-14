e8d959f562abbd4137b8dc9625a00fb61b747817
Andres-15Sanchez <andrettygabo5543@gmail.com>
2024-11-13 21:01:51 -0600

    ciclos

---
+++ Ciclofor.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Andres
+ */
+public class Ciclofor {
+    public static void main(String[] args) {
+        float altura, imc;
+        short peso;
+        for (int resp = 0; resp < 41 ; resp = resp + 1){
+            peso= Short.parseShort(JOptionPane.showInputDialog("Ingresa tu peso"));
+           altura = Float.parseFloat (JOptionPane.showInputDialog("Ingresa tu altura en formato de  metros"));
+            imc = (Float) (peso /(altura * altura)) ;
+            if(imc < 18.49){
+                JOptionPane.showMessageDialog(null,"Peso bajo");
+            }else if( imc >= 18.50 && imc <= 24.99) {
+               JOptionPane.showMessageDialog(null,"Peso Normal"); 
+            }else if (imc >=25 && imc <=29.99){
+                JOptionPane.showMessageDialog(null,"Sobrepeso");
+            }else if(imc>= 30 && imc <= 34.99){
+                JOptionPane.showMessageDialog(null,"Sobrepeso Level");
+            }else if (imc >= 35 && imc <= 39.99){
+                JOptionPane.showMessageDialog(null,"Obesidad Mediana");
+            }else if (imc >= 40){
+                JOptionPane.showMessageDialog(null,"Obesidad Morbida");
+            }
+            resp= Byte.parseByte(JOptionPane.showInputDialog("Ingrese 1 si desea hacer otro calculo"));
+        }
+    }
+}
