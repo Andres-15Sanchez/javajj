c45de9ea260077bbfae2d90cc9ae264f04df4119
Andres-15Sanchez <andrettygabo5543@gmail.com>
2024-11-14 18:47:45 -0600

    factoral

---
+++ factorialdo.java
@@ -0,0 +1,16 @@
+public class DOWHILEfactorial{
+    public static void main(String[] args) {
+        short num;
+        double fact = 1;
+        int x = 1 ;
+        byte resp =1;
+        do{
+            num = (Short)Short.parseShort(JOptionPane.showInputDialog("Ingresa un numero"));
+            for( x = num; x>=1 ; x--){
+                fact = fact * x ;
+            }
+            System.out.print("El factorial del numero" + num + " es "+ fact);
+            resp = (Byte)Byte.parseByte(JOptionPane.showInputDialog("Ingresa el numero 1 si deseas otro calculo"));
+        }while(resp==1);
+    }
+}
