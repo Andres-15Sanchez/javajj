76dddb16c3780e958ef4ee60bc0465411821b0f9
Andres-15Sanchez <andrettygabo5543@gmail.com>
2024-12-05 19:44:59 -0600

    mat

--- MATERIAS.java
+++ MATERIAS.java
@@ -1,37 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 
-public class MATERIAS {
-    public static void main(String[] args) {
-
-         int [] unidad = {
-                    1,
-                    2,
-                    3,
-                    4,
-                    5
-                };
-         
-         System.out.printf("Materias   %4d    %4d    %4d    %4d    %4d" , unidad [0],unidad [1],unidad [2],unidad [3],unidad[4]);
-         
-         String [] materias = {
-                  "FUNINVE",
-                  "CALCDIF",
-                  "QUIMICA",
-                  "FUNDPRO",
-                  "MATEDIS",
-                  "TUTORIA",
-                  "DESUSTA",
-                };
-         
-        System.out.println("\n"+ materias[0] + "\n" + materias [1] + "\n" + materias [2] + "\n" + materias [3] + "\n"
-                + materias [4] + "\n" + materias [5]); 
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Andres
+ */
+public class MATERIAS{
+    public static void main(String[] ANDRES) {
+        //Declaracion de variables
+        String materias[]={"FUNINVE", "CALCDIF","QUIMICA","FUNDPRO","MATEDIS", "TUTORIA"};
+        String encabezado[]={"Materias","U1","U2","U3","U4","U5","Promedio"};
+        byte calificaciones[][]= new byte[7][5]; 
+        byte x,y;
         
-         int [][] calificaciones = {
-            { 85,95,100,70,75},
-            { 100,85,89,96,83},
-            {}
-    };
-         
-    System.out.printf("%2d" + calificaciones [0][1]+  calificaciones [0][2]);     
-    
+        for(x=0;x<=5;x++){//FILAS1
+                for(y=0;y<5;y++){//COLUMNAS
+                   calificaciones[x][y]=Byte.parseByte(JOptionPane.showInputDialog("Ingresa tu calficacion de: " 
+                           + materias[x] + " Unidad " + (y + 1)));
+            }
+        }
+            //IMPRESION DE CALIFICACIONES
+            for(x=0;x<=5;x++){
+                System.out.print(encabezado[x] + "\t");
+            }
+                System.out.println();
+
+                for(x=0;x<=5;x++){
+                    System.out.print(materias[x] + "\t");
+                    for(y=0;y<5;y++){
+                        System.out.print("\t" + calificaciones[x][y]);
+                    }
+                   System.out.println();
+               }
     }
 }
+
