c769b192c71b2f4ed643161d6267baa781e77089
Andres-15Sanchez <andrettygabo5543@gmail.com>
2024-12-16 21:08:41 -0600

    Add files via upload

---
+++ Arreglos.java
@@ -0,0 +1,16 @@
+
+public class Arreglos {
+    public static void main (String[]args){
+        String [] materias = {
+            "Andres",
+            "Jose",
+            "LEOBARDO"
+        };
+        int [] unidad={
+            1,2,3,4
+        };
+        System.out.printf("unidades " + unidad[0]);
+        System.out.println("Estos son los nombres de asistencia: \n" + materias[0]
+        + "\n" + materias[1] + "\n" + materias[2]);
+    }
+}
